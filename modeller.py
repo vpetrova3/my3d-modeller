d1400347224b363515024a8a8f878938a7c35d40
vpetrova3 <vpetrova3@gatech.edu>
2025-06-15 11:25:21 -0400

    Initial 3D modeller setup with PyOpenGL

---
+++ modeller.py
@@ -0,0 +1,25 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import pygame
+from pygame.locals import *
+
+pygame.init()
+display = (800, 600)
+pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+glTranslatef(0.0, 0.0, -5)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glBegin(GL_TRIANGLES)
+    glVertex3f(0.0, 1.0, 0.0)
+    glVertex3f(-1.0, -1.0, 0.0)
+    glVertex3f(1.0, -1.0, 0.0)
+    glEnd()
+    pygame.display.flip()
+    pygame.time.wait(10)
